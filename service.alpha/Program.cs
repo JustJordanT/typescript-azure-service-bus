3ccedcc2c29001638fc5b843e5f23d7dce814f8a
JustJordanT <jordantay9014+master@gmail.com>
2024-09-30 12:40:05 -0500

    feat: init

--- service.alpha/Program.cs
+++ service.alpha/Program.cs
@@ -1,9 +1,18 @@
+using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+using System.Text.Json;
+using service.data;
+using service.servicebus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IServiceBusClient>(sp =>
+    new service.servicebus.ServiceBusClient(builder.Configuration["ServiceBus:ConnectionString"]));
 
 var app = builder.Build();
 
@@ -16,6 +25,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapPost("/message", async (IServiceBusClient serviceBusClient, Events.MessageEventRequest request) =>
+{
+    await serviceBusClient.SendMessageAsync(request, "service.alpha", "1.0.0", "development", "localhost");
+    return Results.Ok();
+});
 
 app.Run();
--- service.beta/Program.cs
+++ service.beta/Program.cs
@@ -1,9 +1,15 @@
+using Azure;
+using Azure.Messaging.ServiceBus;
+using service.servicebus;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IServiceBusClient>(sp =>
+    new service.servicebus.ServiceBusClient(builder.Configuration["ServiceBus:ConnectionString"]));
 
 var app = builder.Build();
 
@@ -14,5 +20,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/message", async (IServiceBusClient serviceBusClient, HttpContext httpContext) =>
+{
+    try
+    {
+        var message = await serviceBusClient.ReceiveMessageAsync();
+        if (message == null)
+        {
+            return Results.NoContent();
+        }
+        return Results.Ok(message);
+    }
+    catch (TimeoutException)
+    {
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(
+            detail: ex.Message,
+            statusCode: StatusCodes.Status500InternalServerError,
+            title: "An error occurred while processing your request"
+        );
+    }
+});
 
 app.Run();
