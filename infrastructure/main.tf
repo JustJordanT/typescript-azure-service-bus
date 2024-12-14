a8ea33aa112ffdf2f56607d90ee2d72ab0eeeaf8
JustJordanT <jordantay9014+master@gmail.com>
2024-12-14 12:19:27 -0600

    updates

--- infrastructure/main.tf
+++
@@ -1,6 +0,0 @@
-module "azure_service_bus" {
-  source = "./modules/azure/service_bus"
-  providers = {
-    azurerm = azurerm
-  }
-}
--- infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "servicebus_queue_name" {
-  value = module.azure_service_bus.servicebus_queue_name
-  sensitive = true
-}
-
-output "servicebus_connection_string" {
-  value = module.azure_service_bus.servicebus_connection_string
-  sensitive = true
-}
